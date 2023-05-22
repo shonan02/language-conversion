17bb73d655909a1004e1498542eb7370568356ef
shonan02 <66680460+shonan02@users.noreply.github.com>
2023-05-22 14:33:18 +0100

    Base project

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-const express = require("express")
-var app = express()
+const express = require("express");
+const app = express();
+
 app.get("/",function(request,response){
 response.send("Hello World!")
-})
+});
+
+
 app.listen(10000, function () {
 console.log("Started application on port %d", 10000)
 });
